7167c9566c448bef61d55c56d883ca386b7fcb3a
José Ángel Sosa López <jasosalopez@outlook.com>
2026-09-11 15:33:07 -0600

    fix: add native astro redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,18 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  redirects: {
+    '/es-home.html': '/es/',
+    '/es-articles.html': '/es/articles/',
+    '/es-author.html': '/es/author/',
+    '/es-ebook.html': '/es/ebook/',
+    '/es-ebook-chapter-01.html': '/es/ebook/01-what-can-industrial-design-teach/',
+    '/home.html': '/',
+    '/articles.html': '/articles/',
+    '/author.html': '/author/',
+    '/ebook.html': '/ebook/',
+    '/ebook-chapter-01.html': '/ebook/01-what-can-industrial-design-teach/',
+    '/ebook/es-ebook.html': '/es/ebook/'
+  }
 });
